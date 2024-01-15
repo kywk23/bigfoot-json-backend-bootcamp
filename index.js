7ca5abdf1808dc91894ea838f80e013693462dc2
kywk23 <kelvinywk.work@outlook.com>
2024-01-16 00:26:30 +0800

    1st commit

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
-const express = require('express')
-const { getSightings } = require('./utils.js')
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+const { getSightings } = require("./utils.js");
+require("dotenv").config();
 
 const PORT = process.env.PORT;
 const app = express();
+app.use(cors());
 
+//DONE & working
 app.get("/sightings", async (req, res) => {
   const sightings = await getSightings();
   res.json(sightings);
 });
+
+app.get("/sightings/:sightingIndex", async (req, res) => {
+  const { sightingIndex } = req.params;
+  const sightings = await getSightings();
+  const sighting = sightings[sightingIndex];
+  res.json(sighting);
+});
+
+// app.get("/sightings/:sightingIndex", async (req, res) => {
+//   const sightings = await getSightings();
+//   res.json(sightings[req.params.sightingIndex]);
+// });
 
 app.listen(PORT, () => {
   console.log(`Express app listening on port ${PORT}!`);
